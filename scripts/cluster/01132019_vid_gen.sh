e0f2d7ca1b97e6768f54652969fcb7eca6f798d2
Prabaha Gangopadhyay <pg.prabaha@gmail.com>
2024-02-21 15:16:52 -0500

    cluster run update

--- scripts/cluster/01132019_vid_gen.sh
+++ scripts/cluster/01132019_vid_gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=01132019_500fps
-#SBATCH --output=cluster/01132019_500fps.out
-#SBATCH --error=cluster/01132019_500fps.err
+#SBATCH --job-name=01132019_1000fps
+#SBATCH --output=cluster/01132019_1000fps.out
+#SBATCH --error=cluster/01132019_1000fps.err
 #SBATCH --partition=psych_week
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=10
--- scripts/cluster/10092018_vid_gen.sh
+++ scripts/cluster/10092018_vid_gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=10092018_500fps
-#SBATCH --output=cluster/10092018_500fps.out
-#SBATCH --error=cluster/10092018_500fps.err
+#SBATCH --job-name=10092018_1000fps
+#SBATCH --output=cluster/10092018_1000fps.out
+#SBATCH --error=cluster/10092018_1000fps.err
 #SBATCH --partition=psych_week
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=10
