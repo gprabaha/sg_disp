c240061b13774d364a777eabc1364d02ee74151b
Prabaha Gangopadhyay <pg.prabaha@gmail.com>
2024-02-22 18:45:13 -0500

    cluster memory update

--- +sg_disp/+util/get_params_for_cluster.m
+++ +sg_disp/+util/get_params_for_cluster.m
@@ -16,8 +16,8 @@ function params = get_params_for_cluster()
     params.video_output_folder      = 'gaze_signal_videos_100fps';
     params.frame_rate               = 100;
     params.video_quality            = 50;
-    params.mem_per_cpu              = 200; % gbs
-    params.num_cpu                  = 10;
+    params.mem_per_cpu              = 250; % gbs
+    params.num_cpu                  = 5;
     % Behavioral file paths
     params.pos_file_list            = pos_file_list;
     params.fix_file_list            = fix_file_list;
