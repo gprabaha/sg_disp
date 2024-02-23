dec6f8c500712278a283aef43602abd4ee4bd51f
Prabaha Gangopadhyay <pg.prabaha@gmail.com>
2024-02-23 16:35:12 -0500

    ffmpeg cluster script update

--- scripts/cluster/01132019_ffmpeg_conv.sh
+++ scripts/cluster/01132019_ffmpeg_conv.sh
@@ -9,8 +9,6 @@
 #SBATCH --time=5:00:00
 #SBATCH --mail-type=FAIL
 
-module load FFmpeg/4.3.1-GCCcore-10.2.0.lua
-
 input_folder="/gpfs/milgram/scratch60/chang/pg496/gaze_signal_videos_100fps"
 output_folder="/gpfs/milgram/scratch60/chang/pg496/gaze_signal_videos_100fps_mp4"
 
@@ -29,7 +27,7 @@ find "$input_subfolder" -type f -name "*.avi" | while IFS= read -r file; do
     output_file="${output_file%.avi}.mp4" #the double percentage is for `compose` to work
     echo "Input_file:$file"
     echo "Output_file:$output_file"
-    ffmpeg -i "$file" -c:v libx265 -crf 28 -preset medium -vf "fps=100" "$output_file" &
+    module load FFmpeg/4.3.1-GCCcore-10.2.0.lua; ffmpeg -i "$file" -c:v libx265 -crf 28 -preset medium -vf "fps=100" "$output_file" &
 done
 
 wait
