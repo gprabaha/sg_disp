80876fec6bceb2ffa3469199bc96955a61c7700d
Prabaha Gangopadhyay <pg.prabaha@gmail.com>
2024-02-23 17:52:03 -0500

    hopefully final scripts for vin gen in cluster and ffmpeg compression thereafter

---
+++ scripts/cluster/01082019_vid_gen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=01082019_fast_iter
+#SBATCH --output=cluster/01082019_fast_iter.out
+#SBATCH --error=cluster/01082019_fast_iter.err
+#SBATCH --partition=psych_week
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=5
+#SBATCH --mem=240G
+#SBATCH --time=7-00:00:00
+#SBATCH --mail-type=FAIL
+
+module load MATLAB/2022b
+
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/gpfs/milgram/project/chang/pg496/repositories/categorical/lib/linux"
+
+matlab -r "addpath( genpath( '/gpfs/milgram/project/chang/pg496/repositories' ) ); session='01082019'; params=sg_disp.util.get_params_for_cluster(); sg_disp.cluster.generate_video_for_one_session( session, params );"
